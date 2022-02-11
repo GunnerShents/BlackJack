9c39a1468f0f01cffffc667517b5f5695163e76f
Python Phil <prshents@hotmail.co.uk>
2022-02-11 23:32:13 +0000

    misc

---
+++ betting.py
@@ -0,0 +1,33 @@
+
+
+#class Chip()
+#constructor (self, chip_value, img)
+#class betting()
+
+class Chip():
+    
+    pass
+    #constructor(self, chip_value, img, x, y)
+        #chip value
+        #img
+        #x
+        #y
+        #chip_number_bet
+    
+    #draw method()
+    
+    #add_chip()
+    
+    #remove_chip()    
+    
+    
+class Betting():
+    
+    pass
+    #constructor(self)
+        #total
+    
+    #get_total
+
+    #set_total
+    
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
-value = 15/2
-value1 = 15//2
 
-value / 2
 
-print (value, value1)
+        
+    
+    
